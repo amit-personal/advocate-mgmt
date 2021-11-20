d69ac429530be284295bd2f479dfd31d6b2dcb6b
Amit <ak9835371292@gmail.com>
2021-11-21 00:05:01 +0530

    ui changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Role.find_or_create_by(name: 'Senior')
+Role.find_or_create_by(name: 'Junior')
+
+State.find_or_create_by(name: 'TN')
